5ec2c886bbb4b84af72011e20a91ced118a923e3
Ronel Rustia <fedstats@yahoo.com>
2017-02-27 15:48:02 -0800

    basic constructor added

---
+++ public/scripts/models/userProfile.js
@@ -0,0 +1,23 @@
+'use strict';
+//image-url, count=5
+function UserProfile(obj) {
+  health: obj.health;
+  dietary: obj.dietary;
+  ingredients:
+}
+
+UserProfile.prototype.getInput(){
+
+}
+
+// $.ajax({
+//   url: `curl "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"`,
+//   method: 'GET',
+//   data: {
+//     author: this.author,
+//     authorUrl: this.authorUrl,
+//     body: this.body,
+//     category: this.category,
+//     publishedOn: this.publishedOn,
+//     title: this.title}
+// });
