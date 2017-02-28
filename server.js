11c0a8a1c4d6b0cdeb0f3c8fb66c319e781fa9cf
Miguel Pena <miguelp1986@gmail.com>
2017-02-28 10:26:54 -0800

    got server up and running with api call

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 'use strict';
 
-const pg = require('pg');
+//const pg = require('pg');
 const express = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// const client = new pg.Client(conString);
-// client.connect();
+//const client = new pg.Client(conString);
+//client.connect(); // connect to DB
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+
+// Routes for requesting HTML resources
+app.get('/', function(request, response) {
+  response.sendFile('index.html', {root: '.'});
+});
+
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}!`);
+});
