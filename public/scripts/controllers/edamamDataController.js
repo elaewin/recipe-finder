ff4b9d4018d76fe10aea409eb9980c55f62cfac0
Miguel Pena <miguelp1986@gmail.com>
2017-02-28 14:07:20 -0800

    removed postgres stuff

--- public/scripts/controllers/edamamDataController.js
+++ public/scripts/controllers/edamamDataController.js
@@ -4,14 +4,22 @@
   const edamamDataController = {};
   edamamDataController.all = [];
 
-  // // JUST TESTING STUFF...
-  // $.get('https://api.edamam.com/search?q=chicken&app_id=8923b38a&app_key=9e829390b2e423e7cee9b5a57834d3ab')
-  // .then(data => console.log(data));
+  // JUST TESTING STUFF...
+  $.get('https://api.edamam.com/search?q=chicken&from=0&to=5&app_id=8923b38a&app_key=9e829390b2e423e7cee9b5a57834d3ab')
+  .then(data => console.log(data));
 
+  // TODO parse out HTTP request
   edamamDataController.requestData = function(callback) {
-    $.get('https://api.edamam.com/search?q=chicken&app_id=8923b38a&app_key=9e829390b2e423e7cee9b5a57834d3ab') // TODO parse this out
-    .then(data => console.log(data), err => console.error(err))
-    .then(callback)
+    $.get('https://api.edamam.com/search?q=chicken&app_id=8923b38a&app_key=9e829390b2e423e7cee9b5a57834d3ab')
+    .then(
+      function(data) {
+        edamamDataController.all = data;
+        console.log(data);
+      },
+      function(err) {
+        console.error(err);
+      })
+      .then(callback);
   }
 
   module.edamamDataController = edamamDataController;
