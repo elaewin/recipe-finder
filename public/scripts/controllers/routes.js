c01c21fa22b70072e13a2dbf5f5f742a95ebb6dd
Ronel Rustia <fedstats@yahoo.com>
2017-03-01 10:32:11 -0800

    sync work branch

---
+++ public/scripts/controllers/routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+page('/about', aboutController.init);
+
+// ingredientName will be an array that will contain all the food ingredient that the user put into the url field.
+page('/recipe/:ingredientName', recipeController.loadAll);
+page('/*', );
+page();
