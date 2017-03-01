a87193f2fd81bdf7338733de165f4dd9ddb7c43f
Ronel Rustia <fedstats@yahoo.com>
2017-02-28 17:15:52 -0800

    commented out fetchall temporarily

--- public/scripts/models/recipe.js
+++ public/scripts/models/recipe.js
@@ -10,12 +10,9 @@ Recipe.all = [];
 
 Recipe.loadAll = () => {
   Recipe.all = edamamDataController.all.map(ele => new Recipe(ele));
-  // localStorage.rawData.push(edamamDataController.all[edamamDataController.all.indexOf($(‘q’) + 1]);
-  // localStorage.rawData.push(edamamDataController.all[edamamDataController.all.indexOf($(‘url’) + 1]);
-  // localStorage.rawData.push(edamamDataController.all[edamamDataController.all.indexOf($(‘image’) + 1]);
 };
 
-// Recipe.fetchAll = callback => {
+Recipe.fetchAll = callback => {
 // //TODO: fix foo data here
 //   $.get('/foo')
 //   .then(
@@ -25,24 +22,18 @@ Recipe.loadAll = () => {
 //         callback();
 //       } else {
 // // TODO: fix this fall-back after we decide on a behaviour for this
-//         $.ajax({
-//           url: '',
-//           method: 'GET',
-//
-//         })
 //         // $.getJSON(localStorage.rawData)
 //         // .then(rawData => {
 //         //   rawData.forEach(item => {
 //         //     let recipe = new Recipe(item);
 //         //     recipe.insertRecord();
 //           })
-//         }
 //         .then(() => Recipe.fetchAll(callback))
 //         .catch(console.error);
 //       }
 //     }
 //   )
-// };
+};
 
 
   module.Recipe = Recipe;
