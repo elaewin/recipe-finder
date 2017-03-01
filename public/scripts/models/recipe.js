ce100698dd41196395898afcde4d9f3e58fa0828
Ronel Rustia <fedstats@yahoo.com>
2017-02-28 17:00:51 -0800

    recipe js implementation

--- public/scripts/models/recipe.js
+++ public/scripts/models/recipe.js
@@ -1,40 +1,48 @@
 'use strict';
-
+(function(module){
 function Recipe(obj) {
-  label: obj.label;
-  image: obj.image;
-  url: obj.url;
+  this.label = obj.recipe.label;
+  this.image = obj.recipe.image;
+  this.url = obj.recipe.url;
 }
 
-recipe.all = [];
+Recipe.all = [];
 
-Recipe.loadAll = rows => {
-  recipe.all = rows.map(ele => new Recipe(ele));
+Recipe.loadAll = () => {
+  Recipe.all = edamamDataController.all.map(ele => new Recipe(ele));
+  // localStorage.rawData.push(edamamDataController.all[edamamDataController.all.indexOf($(‘q’) + 1]);
+  // localStorage.rawData.push(edamamDataController.all[edamamDataController.all.indexOf($(‘url’) + 1]);
+  // localStorage.rawData.push(edamamDataController.all[edamamDataController.all.indexOf($(‘image’) + 1]);
 };
 
-Recipe.fetchAll = callback => {
-//TODO: fix foo data here
-  $.get('/foo')
-  .then(
-    results => {
-      if (results.length) {
-        recipe.loadAll(results);
-        callback();
-      } else {
-// TODO: fix this fall-back after we decide on a behaviour for this
-        // $.getJSON(localStorage.recipe)
-        // .then(rawData => {
-        //   rawData.forEach(item => {
-        //     let recipe = new recipe(item);
-        //     recipe.insertRecord();
-          })
-        })
-        .then(() => recipe.fetchAll(callback))
-        .catch(console.error);
-      }
-    }
-  )
-};
+// Recipe.fetchAll = callback => {
+// //TODO: fix foo data here
+//   $.get('/foo')
+//   .then(
+//     results => {
+//       if (results.length) {
+//         Recipe.loadAll(results);
+//         callback();
+//       } else {
+// // TODO: fix this fall-back after we decide on a behaviour for this
+//         $.ajax({
+//           url: '',
+//           method: 'GET',
+//
+//         })
+//         // $.getJSON(localStorage.rawData)
+//         // .then(rawData => {
+//         //   rawData.forEach(item => {
+//         //     let recipe = new Recipe(item);
+//         //     recipe.insertRecord();
+//           })
+//         }
+//         .then(() => Recipe.fetchAll(callback))
+//         .catch(console.error);
+//       }
+//     }
+//   )
+// };
 
 
   module.Recipe = Recipe;
