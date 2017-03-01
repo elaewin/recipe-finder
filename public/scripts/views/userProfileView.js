1aaba4cbcdecda0f70833acc48994c014e33340f
Disa <justdisa@gmail.com>
2017-03-01 14:37:05 -0800

    moving code from controller to view

--- public/scripts/views/userProfileView.js
+++ public/scripts/views/userProfileView.js
@@ -1,10 +1,33 @@
 'use strict';
 
-(function (module) {
-  const userProfileView = {};
+(function(module) {
+  const userProfileController = {};
 
-  const userProfile = function()
+  userProfileController.index = () => {
+    $('main section').hide();
+    $('#intro').show().children().hide();
 
+    // show proper intro message, depending if user is returning user
+    if (localStorage.rawData) {
+      $('.returning-visit').show();
+      $('button.returning-visit').on('click', function(){
+        if (this.text() === 'YES') {
+          // TODO write code skip the health and diet prompt to the keyword prompt.
+        } else {
+          // TODO write code to proceed to health and diet prompt
+        }
+      })
+    } else {
+      $('#first-visit').show();
+    }
+    $('#health-and-diet-prompt').on('click', function(){
+      if (this.text() === 'YES') {
+        // TODO: write code to proceed to health and diet
+      } else {
+        // TODO: write code to skip to the ingredient/keyword prompt
+      }
+    })
+  };
 
-  module.userProfileView = userProfileView;
+  module.userProfileController = userProfileController;
 })(window);
