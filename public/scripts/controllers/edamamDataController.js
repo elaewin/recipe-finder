a37dfb0d4cfd0a88b3c642f84b6b6120b8f6dc91
Miguel Pena <miguelp1986@gmail.com>
2017-03-01 13:51:49 -0800

    parsed out http request

--- public/scripts/controllers/edamamDataController.js
+++ public/scripts/controllers/edamamDataController.js
@@ -6,12 +6,12 @@
 
   edamamDataController.getHTTPRequest = function() {
     // TODO: fill in placeholders
-    var httpRequest = `https://api.edamam.com/search?q=${/*GET KEYWORDS FROM HTML*/}&app_id=${APP_ID}&app_key=${APP_KEY}&healthLabels=${/*GET HEALTH LABELS FROM HTML*/}&dietLabels=${/*GET DIET LABELS FROM HTML*/}`;
+    return `https://api.edamam.com/search?q=${/*GET KEYWORDS FROM HTML*/}&app_id=${APP_ID}&app_key=${APP_KEY}&healthLabels=${/*GET HEALTH LABELS FROM HTML*/}&dietLabels=${/*GET DIET LABELS FROM HTML*/}`;
   }
 
   // TODO parse out HTTP request
   edamamDataController.requestData = function(callback) {
-    $.get('https://api.edamam.com/search?q=chicken&app_id=8923b38a&app_key=9e829390b2e423e7cee9b5a57834d3ab')
+    $.get(edamamDataController.getHTTPRequest)
     .then(
       function(data) {
         edamamDataController.all = data.hits;
