b78943def83cc87183480ef5ba45d2338a241347
Disa <justdisa@gmail.com>
2017-03-02 10:22:56 -0800

    working on getting values from checkboxes

--- public/scripts/models/userProfile.js
+++ public/scripts/models/userProfile.js
@@ -1,5 +1,6 @@
-'use stict';
-
+'use strict';
+(function(module) {
+  const userProfile = {};
 // user profile constructor
 function UserProfile (object) {
   this.health = object.health;
@@ -7,6 +8,9 @@ function UserProfile (object) {
   this.keyword = object.keyword
 }
 
-UserProfile.health = [dummy1, dummy2, dummy3];
-UserProfile.diet = [dummy4, dummy5, dummy6];
-UserProfile.keyword = [dummy7, dummy8, dummy9];
+UserProfile.health = [];
+UserProfile.diet = [];
+UserProfile.keyword = [];
+
+module.userProfile = userProfile;
+})(window);
--- public/scripts/views/userProfileView.js
+++ public/scripts/views/userProfileView.js
@@ -1,24 +1,24 @@
 'use strict';
 
 (function(module) {
-  const userProfileController = {};
+  const userProfileView = {};
 
-  userProfileController.index = () => {
-    $('main section').hide();
+  // userProfileView.index = () => {
+  //   $('main section').hide();
 
     // show proper intro message, depending if user is returning user
-    if (/*function to check localStorage*/) {
-      $('.returning-visit').fadeIn();
-      $('button.returning-visit').on('click', function(){
-        if (this.text() === 'YES') {
-          $('main section').hide();
-          $('#keyword-prompt').fadeIn();
-          // DONE write code skip the health and diet prompt to the keyword prompt.
-        } else if (this.text() === 'NO') {
-          $('main section').hide();
-          $('#health-and-diet-prompt')fadeIn();
-        }
-      })
+    // if (/*function to check localStorage*/) {
+    //   $('.returning-visit').fadeIn();
+    //   $('button.returning-visit').on('click', function(){
+    //     if (this.text() === 'YES') {
+    //       $('main section').hide();
+    //       $('#keyword-prompt').fadeIn();
+    //       // DONE write code skip the health and diet prompt to the keyword prompt.
+    //     } else if (this.text() === 'NO') {
+    //       $('main section').hide();
+    //       $('#health-and-diet-prompt')fadeIn();
+    //     }
+    //   })
 
 
 
@@ -26,49 +26,58 @@
 
 
 
-          $('main section').hide();
-          $('#health-and-diet-prompt').fadeIn();
-            if (this.text) {
-              $('button#health-yes').on('click', function(){
-                $('main section').hide();
-                $('#health-and-diet').fadeIn();
-              } else {
-                $('main section').hide();
-                $('#keyword-prompt').fadeIn();
-              })
+  //         $('main section').hide();
+  //         $('#health-and-diet-prompt').fadeIn();
+  //           if (this.text) {
+  //             $('button#health-yes').on('click', function(){
+  //               $('main section').hide();
+  //               $('#health-and-diet').fadeIn();
+  //             } else {
+  //               $('main section').hide();
+  //               $('#keyword-prompt').fadeIn();
+  //             })
+  //
+  //           }
+  //         // DONE write code to proceed to health and diet prompt
+  //       }
+  //     })
+  //   } else {
+  //     $('#first-visit').show();
+  //   }
+  //   $('#health-and-diet-prompt').on('click', function(){
+  //     if (this.text() === 'YES') {
+  //       // TODO: write code to proceed to health and diet
+  //     } else {
+  //       // TODO: write code to skip to the ingredient/keyword prompt
+  //     }
+  //   })
+  // };
+  $('#health-input').submit(function(event){
+    console.log('Inside health-input submit');
+    event.preventDefault();
+  });
 
-            }
-          // DONE write code to proceed to health and diet prompt
-        }
-      })
-    } else {
-      $('#first-visit').show();
-    }
-    $('#health-and-diet-prompt').on('click', function(){
-      if (this.text() === 'YES') {
-        // TODO: write code to proceed to health and diet
-      } else {
-        // TODO: write code to skip to the ingredient/keyword prompt
-      }
+  $('#health-check-submit').on('click', function(event) {
+    event.preventDefault();
+    console.log('health-check button clicked');
+    $('.health-check:checked').each(function(){
+       UserProfile.health.push($(this).val);
     })
-  };
+    console.log('health profile', UserProfile.health);
+  });
 
-  module.userProfileController = userProfileController;
+
+
+
+  // $('.diet-check:checked').each(function(){
+  //    UserProfile.diet.push($(this).val);
+  // })
+  //
+  // $('.keyword-prompt button').on('click', function() {
+  //   $('#keyword-form').find(':input').each(function(){
+  //   UserProfile.keyword.push($(this).val());
+  // });
+  // });
+
+  module.userProfileView = userProfileView;
 })(window);
-
-
-****to get user data into arrays****
-
-$('.health-check:checked').each(function(){
-   UserProfile.health.push($(this).val);
-})
-
-$('.diet-check:checked').each(function(){
-   UserProfile.diet.push($(this).val);
-})
-
-$('.keyword-prompt button').on('click', function() {
-  $('#keyword-form').find(':input').each(function(){
-  UserProfile.keyword.push($(this).val());
-});
-});
